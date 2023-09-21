03cbfda3ebe85ad9b77b883b4beb88d283a8d8f4
Brandon Calabrese <thirtyvirus1@gmail.com>
2023-09-21 11:26:54 -0400

    started working on v2.9

--- src/main/java/thirtyvirus/uber/events/chat/TabComplete.java
+++ src/main/java/thirtyvirus/uber/events/chat/TabComplete.java
@@ -4,8 +4,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
 import thirtyvirus.uber.UberItems;
 
 import java.util.ArrayList;
